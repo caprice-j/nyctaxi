4b591e7774aa0e76a2cd74265adbad21b7d72038
caprice-j <skyfly0008@gmail.com>
2016-12-14 08:24:25 +0000

    add apply-random-forest.py

---
+++ SPARK/execute_random_forest.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+EXE=spark-submit
+FILENAME=apply-random-forest.py
+NTREE=50
+TYPE1=before
+TYPE2=during
+TYPE3=finished
+
+if [ "$1" = "before" ]; then
+    echo 1
+    nohup ${EXE} ${FILENAME} ${TYPE1} ${NTREE} > ${FILENAME}.${TYPE1}.out 2>&1&
+elif [ "$1" = "during" ]; then
+    echo 2
+    nohup ${EXE} ${FILENAME} ${TYPE2} ${NTREE} > ${FILENAME}.${TYPE2}.out 2>&1&
+else
+    echo 3
+    nohup ${EXE} ${FILENAME} ${TYPE3} ${NTREE} > ${FILENAME}.${TYPE3}.out 2>&1&
+fi
