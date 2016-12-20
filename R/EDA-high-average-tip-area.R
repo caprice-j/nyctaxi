f086ef82ece7ef5d0cfd603b61c26916f4a841bc
caprice-j <skyfly0008@gmail.com>
2016-12-20 17:43:53 -0500

    update

--- R/EDA-high-average-tip-area.R
+++ R/EDA-high-average-tip-area.R
@@ -16,5 +16,11 @@
                                  xmax=px4+sz, ymax=py4+sz,fill=as.factor(averageTip>=3))) + mylab + scale_fill_manual(guide =guide_legend(title="averageTip\n >= $3"), values = c("#142C42", "#5AB3F5") ) + myscale
  dev.off()
  
-
+ png("EDA/univ1month-averageTipOver3-twoColor.png", width=960, height=1920)
+ ggplot(aggregated) + geom_rect(aes(xmin=px4-sz, ymin=py4-sz,
+                                    xmax=px4+sz, ymax=py4+sz,fill=as.factor(highRate>=.5))) + labs(title="Area with hpay>=12 trip records exceeding the half") + scale_fill_manual(guide =guide_legend(title=">50% trips\nhpay>=$12"), values = c("#142C42", "#5AB3F5") ) + myscale
+ dev.off()
+ 
+ 
+ 
  
